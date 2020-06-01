7624143a0a29ca0e9bfb2542d6ded4bb126da4aa
cboulafaa <boulafaacharaf@gmail.com>
2020-06-01 19:42:19 -0400

    add Search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Search.css'
+
+export default function Search() {
+  return (
+    <div className="search-form">
+      <input type="search" id="search-input" />
+      <button>Search</button>
+    </div>
+  )
+}
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Main.css'
+import Search from '../../components/Search/Search'
 
 export default function Main() {
   return (
-    <div>
-      Main Page
-    </div>
+    <main>
+      <Search />
+    </main>
   )
 }
