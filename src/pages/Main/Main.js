b9e09d72a2f7f1b613cec1d4fd59d05b9b67acbc
cboulafaa <boulafaacharaf@gmail.com>
2020-06-01 15:39:18 -0400

    main page setup

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+
+export default function Main() {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  )
+}
