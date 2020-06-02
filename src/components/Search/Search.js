b83b8b69c8e0b27d2e8331ae59b5aea208401f51
cboulafaa <boulafaacharaf@gmail.com>
2020-06-02 09:03:39 -0400

    Search component logic

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
+import axios from 'axios'
 
-export default function Search() {
+export default function Search(props) {
+
+  const [query, setQuery] = useState('')
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault()
+
+    const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${query}&type=playlist&key=${process.env.REACT_APP_API_KEY}`)
+
+    console.log(res.data.items)
+
+  }
+
+  const handleChange = (e) => setQuery(e.target.value)
+
   return (
-    <div className="search-form">
-      <input type="search" id="search-input" />
-      <button>Search</button>
-    </div>
+    <form className="search-form">
+      <input type="search" id="search-input" onChange={handleChange} />
+      <button type='submit' onClick={handleSubmit}>Search</button>
+    </form>
   )
 }
