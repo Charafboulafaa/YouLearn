0ddcd1e13294d9a7d33e951b0ff0e29b4fc90606
cboulafaa <boulafaacharaf@gmail.com>
2020-06-02 16:31:06 -0400

    add course object to localstorage on add click

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -4,17 +4,26 @@ import { Card, Button, CardColumns } from 'react-bootstrap';
 
 
 export default function SearchResults(props) {
+
+  const handleClick = (e, course) => {
+    let arr = localStorage.getItem('myCourses') != null ? JSON.parse(localStorage.getItem('myCourses')) : []
+
+    arr.push(course)
+
+    localStorage.setItem('myCourses', JSON.stringify(arr))
+  }
+
   return (
     <div className="SearchResults">
       <CardColumns>
         {props.coursesList.map(course => {
           return (
-            <Card style={{ width: '18rem' }}>
+            <Card >
               <Card.Img variant="top" src={course.snippet.thumbnails.high.url} />
               <Card.Body>
                 <Card.Title>{course.snippet.title}</Card.Title>
                 <Card.Text>{course.snippet.description.slice(0, 50)}</Card.Text>
-                <Button variant="outline-info" size="lg" block>Add to my courses</Button>
+                <Button onClick={(e) => handleClick(e, course)} variant="outline-info" size="lg" block>Add to my courses</Button>
               </Card.Body>
             </Card>
           )
