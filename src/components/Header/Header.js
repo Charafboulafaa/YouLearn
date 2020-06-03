dfa327f36e462ac834fd860e2bfe7a720b064642
cboulafaa <boulafaacharaf@gmail.com>
2020-06-03 12:27:28 -0400

    redesign header.js with Router Links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Header.css'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header>
-      <h1><span>You</span>Learn</h1>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <h1><Link to="/">You<span>Learn</span></Link></h1>
+
+          <Nav className="ml-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/mycourses">My Courses</Link>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </header>
   )
 }
