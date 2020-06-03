355bb4087deaedd6f6c30ff9ea9d878f6f3f1f13
cboulafaa <boulafaacharaf@gmail.com>
2020-06-03 11:41:57 -0400

    add React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Main from './pages/Main/Main'
+import MyCourses from './pages/MyCourses/MyCourses'
 import Footer from './components/Footer/Footer'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Main />
+
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/mycourses">
+          <MyCourses />
+        </Route>
+      </Switch>
+
       <Footer />
-    </>
+    </Router>
   )
 }
