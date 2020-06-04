f1e45a0d40c7f63147fa8537a5550f25b212a927
cboulafaa <boulafaacharaf@gmail.com>
2020-06-04 08:49:56 -0400

    Learn page design & show videos list

--- src/pages/Learn/Learn.js
+++ src/pages/Learn/Learn.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { Container, Row, Col } from 'react-bootstrap'
+
+import './Learn.css'
+
 
 export default function Learn() {
 
+  const [course, setCourse] = useState({})
+
   let { courseId } = useParams()
 
+  useEffect(() => {
+    const apiCall = async () => {
+      const res = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${courseId}&key=${process.env.REACT_APP_API_KEY}`)
+
+      setCourse(res.data)
+    }
+
+    apiCall()
+  }, [])
+
   return (
-    <div>
-      {courseId}
-    </div>
+    <main id="learn" className="py-5">
+      <Container>
+
+        <Row>
+          <Col xs={8}>
+            Player
+          </Col>
+
+          <Col xs={4}>
+            <Container>
+              {course.items && course.items.map((el) => {
+                return (<Row className="my-2">
+                  <Col><img src={el.snippet.thumbnails.medium.url} /></Col>
+                  <Col>
+                    <div className="font-weight-bold">{el.snippet.title.slice(0, 40) + '...'}</div>
+                  </Col>
+
+                </Row>)
+              })}
+            </Container>
+          </Col>
+        </Row>
+
+      </Container>
+    </main>
   )
 }
