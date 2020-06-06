35ce71f81db980fdada5f27b00febdc9b45e4b42
cboulafaa <boulafaacharaf@gmail.com>
2020-06-05 22:05:24 -0400

    details description \n back to line solve

--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -2,10 +2,17 @@ import React from 'react'
 import './VideoDetails.css'
 
 export default function VideoDetails(props) {
+
+  const { title, description } = props.details
+
   return (
     <div>
-      <h1>{props.details.title}</h1>
-      <p>{props.details.description}</p>
+      <h1>{title}</h1>
+      <p>{description && description.split('\n').map(i => {
+        return <p>{i}</p>
+      })}</p>
+
+
     </div>
   )
 }
